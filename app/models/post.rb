20f844328022ffd075da01b901a3e74493cfbc14
rsundar <rohan.s.1987@gmail.com>
2020-02-13 03:12:04 +0530

    Improved the url validation regex

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 
     validates :body, length: { maximum: 255 }, presence: true
-    VALID_URL_REGEX = /\A((http|https):\/{2})([a-zA-Z\d]{3})(\.[a-zA-Z\d\-]+)+\.(com|org|edu)\z/
+    VALID_URL_REGEX = /\A((http|https):\/{2})([a-zA-Z\d]{3})(\.[a-zA-Z\d\-]+)+\.(com|org|edu|gov)\z/
     validates :post, length: { maximum: 255 }, presence: true, format: { with: VALID_URL_REGEX }
 end
