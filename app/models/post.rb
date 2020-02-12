564d7cdb90fed57d0363d67e99304ed09cde2c1e
Mario Barrios <mariobarriossalazar@outlook.com>
2020-02-12 15:21:07 -0600

    Added test suite for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
 
     validates :body, length: { maximum: 255 }, presence: true
     VALID_URL_REGEX = /\A((http|https):\/{2})([a-zA-Z\d]{3})(\.[a-zA-Z\d\-]+)+\.(com|org|edu)\z/
-    validates :post, length: { maximum: 255 }, presence: true
+    validates :post, length: { maximum: 255 }, presence: true, format: { with: VALID_URL_REGEX }
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = Post.new(body: 'Lorem Ipsum', post: 'https://www.google.com')
+  end
+
+  test "Should be valid post" do
+    assert @post.valid?
+  end
+
+  test "Body should be present" do
+    @post.body = ''
+    assert_not @post.valid?
+  end
+
+  test "Url should be present" do
+    @post.post = ''
+    assert_not @post.valid?
+  end
 end
