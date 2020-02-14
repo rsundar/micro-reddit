fbddb006ccdd2e336943597613c443fe9ed7730d
rsundar <rohan.s.1987@gmail.com>
2020-02-14 19:33:25 +0530

    refactored VALID_URL_REGEX

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 
     validates :body, length: { maximum: 255 }, presence: true
-    VALID_URL_REGEX = /\A((http:\/{2})|(https:\/{2}))([a-zA-Z\d]{3}\.)(([a-zA-Z\d\-]+\.)+|(\.[a-zA-Z\d\-]+\.))(com|org|edu|gov|in|au|us)(\/[\w\d\-\?\=\+\&\.]+)*(\.[a-zA-Z]+)*\z/ix
+    VALID_URL_REGEX = /\A((http:\/{2})|(https:\/{2}))([a-zA-Z\d]{3}\.)(([a-zA-Z\d\-]+\.)+|(\.[a-zA-Z\d\-]+\.))(com|org|edu|gov|in|au|us)(\/[\w\d\W]+)*(\.[a-zA-Z]+)*\z/ix
     validates :post, length: { maximum: 255 }, presence: true, format: { with: VALID_URL_REGEX }
 end
