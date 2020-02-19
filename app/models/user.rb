b5de3df5d18228a4824ec857d6ba7cce6dd192eb
rsundar <rohan.s.1987@gmail.com>
2020-02-19 22:09:25 +0530

    added one to many association to user model with posts and comments.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :comment 
+    has_many :post
     before_save { self.email = email.downcase }
     validates :name, presence: true
     VALID_EMAIL = /\A[\w\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
