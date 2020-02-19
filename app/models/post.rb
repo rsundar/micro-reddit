659312a4a472276bba968de3ee12411197c36089
rsundar <rohan.s.1987@gmail.com>
2020-02-19 22:10:45 +0530

    added one to many association between post and comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-
+    has_many :comment
     validates :body, length: { maximum: 255 }, presence: true
     VALID_URL_REGEX = /\A((http:\/{2})|(https:\/{2}))([a-zA-Z\d]{3}\.)(([a-zA-Z\d\-]+\.)+|(\.[a-zA-Z\d\-]+\.))(com|org|edu|gov|in|au|us)(\/[\w\d\W]+)*(\.[a-zA-Z]+)*\z/ix
     validates :post, length: { maximum: 255 }, presence: true, format: { with: VALID_URL_REGEX }
